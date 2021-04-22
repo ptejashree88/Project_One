abbfdd44dc3b3e2c26952649544559e59b4c665c
Vishnuworld123 <vrbhandari1995@gmail.com>
2021-04-22 23:22:09 +0530

    added custome ser and custom models

--- bookinfo/models.py
+++ bookinfo/models.py
@@ -27,3 +27,13 @@ def my_gen():
     yield 2
     yield 3
     yield 4
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
--- bookinfo/serializers.py
+++ bookinfo/serializers.py
@@ -6,3 +6,18 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         exclude = ['is_deleted']
+
+
+
+class CustomSerializer(serializers.Serializer):
+    a_model_serializer = AModelSerializer()
+    b_model_serializer = serializers.ListField(child=CModelSerializer())
+    another_field = serializers.CharField()
+
+    def create(self, validated_data):
+       print(validated_data)
+       # Do your custom create
+
+    def update(self, instance, validated_data):
+       print(validated_data)
+       # Do your custom update
